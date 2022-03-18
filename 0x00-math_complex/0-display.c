3b60c0a32b29c2b7fa0630bb9fe0f897efee484b
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-18 07:18:16 -0700

    task 0

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -6,19 +6,55 @@
  */
 void display_complex_number(complex c)
 {
-	if (c.re == 0 && c.im == 0)
-		printf("0\n");
-	else
-		if (c.re == 0)
-			printf("%gi\n", c.im);
-		else
-			if (c.im == 0)
-				printf("%g\n", c.re);
+if (c.re == 0 && c.im == 0)
+	printf("0\n");
+else
+if (c.re != 0)
+	{
+		printf("%g", c.re);
+		if (c.im > 1)
+			printf(" + %gi\n", c.im);
+		else 
+			if (c.im == 1)
+				printf(" + i\n");
 			else
-				if (c.im == 1)
-					printf("%g + i\n", c.re);
+				if (c.im < 0)
+					{
+						c.im = -c.im;
+						if (c.im == 1)
+							printf(" - i\n");
+						else
+							printf(" - %gi", c.im);
+					}
 				else
-					printf("%g + %gi\n", c.re, c.im);
+					if (c.im == 0)
+						printf("\n");
+	}
+else
+	if (c.re == 0)
+	{
+		if (c.im > 1)
+			printf(" + %gi\n", c.im);
+		else
+			if (c.im == 1)
+				printf(" + i\n");
+		        else
+				if (c.im < 0)
+				{
+					c.im = -c.im;
+					if (c.im == 1)
+						printf(" - i\n");
+					else
+						printf(" - %gi", c.im);
+				}
+	}	
+		
+	
+
+
+
+
+
 
 }
 
