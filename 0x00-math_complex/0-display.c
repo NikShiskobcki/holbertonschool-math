2302d4f682738a7e4c08af20d2b63f77c92084ab
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-18 07:22:46 -0700

    task 0

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -4,57 +4,35 @@
  * display_complex_number - displays complex number
  *  @c: input
  */
+
 void display_complex_number(complex c)
 {
-if (c.re == 0 && c.im == 0)
-	printf("0\n");
-else
-if (c.re != 0)
-	{
+	if (!c.re && !c.im)
+		printf("0\n");
+
+	if (c.re != 0)
 		printf("%g", c.re);
-		if (c.im > 1)
-			printf(" + %gi\n", c.im);
-		else 
-			if (c.im == 1)
-				printf(" + i\n");
-			else
-				if (c.im < 0)
-					{
-						c.im = -c.im;
-						if (c.im == 1)
-							printf(" - i\n");
-						else
-							printf(" - %gi", c.im);
-					}
-				else
-					if (c.im == 0)
-						printf("\n");
-	}
-else
-	if (c.re == 0)
+
+	if (c.im > 0 && c.re != 0)
 	{
-		if (c.im > 1)
-			printf(" + %gi\n", c.im);
+		printf(" + ");
+
+		if (c.im == 1)
+			printf("i\n");
 		else
-			if (c.im == 1)
-				printf(" + i\n");
-		        else
-				if (c.im < 0)
-				{
-					c.im = -c.im;
-					if (c.im == 1)
-						printf(" - i\n");
-					else
-						printf(" - %gi", c.im);
-				}
-	}	
-		
-	
+			printf("%gi\n", c.im);
+	}
 
+	if (c.im < 0 && c.re != 0)
+	{
+		printf(" - ");
 
+		if (c.im == -1)
+			printf("i\n");
+		else
+			printf("%gi\n", c.im * -1);
+	}
 
-
-
-
+	if (c.im == 0)
+		printf("\n");
 }
-
