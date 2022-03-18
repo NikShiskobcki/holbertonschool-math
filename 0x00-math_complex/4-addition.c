5207410f13cbc1db9c109c474ddc2121761b5e53
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-18 10:02:17 -0700

    task 4

---
+++ 0x00-math_complex/4-addition.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * addition - adds
+ * @c1: input
+ * @c2: input
+ * @c3: input
+ */
+void addition(complex c1, complex c2, complex *c3)
+{
+	c3.re = c1.re + c2.re;
+       	c3.im = c1.im + c2.im;	
+}
