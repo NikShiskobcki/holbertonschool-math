af2d993f3364ea59c58af21656e28dac1a1eaf32
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-03-18 06:43:04 -0700

    task 0

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -15,7 +15,10 @@ void display_complex_number(complex c)
 			if (c.im == 0)
 				printf("%g\n", c.re);
 			else
-				printf("%g + %gi\n", c.re, c.im);
+				if (c.im == 1)
+					printf("%g + i\n", c.re);
+				else
+					printf("%g + %gi\n", c.re, c.im);
 
 }
 
